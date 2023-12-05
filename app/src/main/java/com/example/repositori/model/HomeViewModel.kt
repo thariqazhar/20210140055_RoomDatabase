a70c148dc317e81b348db5811a6e106f5ccf0fb3
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 06:43:02 +0700

    Create data class HomeUIState

--- app/src/main/java/com/example/repositori/model/HomeViewModel.kt
+++ app/src/main/java/com/example/repositori/model/HomeViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.repositori.model
 
 import androidx.lifecycle.ViewModel
+import com.example.repositori.data.Siswa
 import com.example.repositori.repo.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
