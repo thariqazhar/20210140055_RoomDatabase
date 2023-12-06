63db4802694ee147e8f1f20a04cd36d7f4eeb9fb
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 10:54:50 +0700

    Create file PengelolaHalaman.kt in navigasi package and creating fun SiswaTopAppBar

---
+++ app/src/main/java/com/example/repositori/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,18 @@
+package com.example.repositori.navigasi
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+
+}
--- app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
@@ -1,10 +1,13 @@
 package com.example.repositori.ui.theme.halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.repositori.R
 import com.example.repositori.model.EntryViewModel
