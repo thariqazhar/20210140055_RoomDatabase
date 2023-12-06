4d7fccf640bcea2998d14934020b0ea7b7ba2bc2
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 10:48:59 +0700

    Adding val for coroutineScope and scroll behavior

--- app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
@@ -1,7 +1,9 @@
 package com.example.repositori.ui.theme.halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.repositori.R
@@ -21,6 +23,7 @@ fun EntrySiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
 
