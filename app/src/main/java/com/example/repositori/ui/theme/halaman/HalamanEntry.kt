e7d78052e605bbe830c96c3c31bcb9adf9d6a60e
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 10:48:02 +0700

    Create fun EntrySiswaScreen

--- app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
@@ -1,9 +1,26 @@
 package com.example.repositori.ui.theme.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.repositori.R
+import com.example.repositori.model.EntryViewModel
+import com.example.repositori.model.PenyediaViewModel
 import com.example.repositori.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
+
