5834392fc567672ff9fbba5b696914a6a73460ee
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 11:25:45 +0700

    Add outlined text field for alamat

--- app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
@@ -57,5 +57,15 @@ fun FormInputSiswa(
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled
         )
+
+        OutlinedTextField(
+            value = detailSiswa.alamat,
+            onValueChange ={onValueChange(detailSiswa.copy(alamat = it))},
+            label = { Text(stringResource(R.string.alamat)) },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled
+        )
+
     }
 }
