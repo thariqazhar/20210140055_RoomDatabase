d06adfe03bc66bc5943e75313a9c4a91d692de6b
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 11:18:58 +0700

    Create fun FormInputSiswa on HalamanEntry add a column for Outlined text fields

--- app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
@@ -1,17 +1,21 @@
 package com.example.repositori.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.repositori.R
+import com.example.repositori.model.DetailSiswa
 import com.example.repositori.model.EntryViewModel
 import com.example.repositori.model.PenyediaViewModel
+import com.example.repositori.model.UIStateSiswa
 import com.example.repositori.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
@@ -30,3 +34,18 @@ fun EntrySiswaScreen(
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+
+    }
+}
