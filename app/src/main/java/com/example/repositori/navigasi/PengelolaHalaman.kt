410223e71d0ebc93538c0e52060132dc0d972886
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 10:56:37 +0700

    Aligning TopAppBar to the center

--- app/src/main/java/com/example/repositori/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/repositori/navigasi/PengelolaHalaman.kt
@@ -1,9 +1,17 @@
 package com.example.repositori.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.repositori.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -14,5 +22,19 @@ fun SiswaTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
 ) {
-
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.back)
+                    )
+                }
+            }
+        }
+    )
 }
