77e5da466b31d6c403aebb410fcf7f990431940e
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 16:15:19 +0700

    Add android:name in AndroidManifest, change compileSDK to 34, and Changging Greeting() to SiswaApp() in MainActivity

--- app/src/main/java/com/example/repositori/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/repositori/navigasi/PengelolaHalaman.kt
@@ -14,11 +14,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.repositori.R
 import com.example.repositori.ui.theme.halaman.DestinasiEntry
 import com.example.repositori.ui.theme.halaman.DestinasiHome
 import com.example.repositori.ui.theme.halaman.EntrySiswaScreen
 import com.example.repositori.ui.theme.halaman.HomeScreen
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
