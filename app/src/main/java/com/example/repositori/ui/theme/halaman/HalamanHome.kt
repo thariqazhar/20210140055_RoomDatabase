e10caafc0d62178218c80f4ff8d7d891dd5b4613
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 15:30:33 +0700

    Add text and icon to DataSiswa card

--- app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanHome.kt
@@ -3,10 +3,16 @@ package com.example.repositori.ui.theme.halaman
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -35,8 +41,26 @@ fun DataSiswa(
             Row(
                 modifier = Modifier.fillMaxWidth()
             ) {
+                Text(
+                    text = siswa.nama,
+                    style = MaterialTheme.typography.titleLarge
+                )
 
+                Spacer(Modifier.weight(1f))
+
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = siswa.telpon,
+                    style = MaterialTheme.typography.titleMedium,
+                )
             }
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
 }
