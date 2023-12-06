8e9a59ded5fd762527de7b86e76594206f244d88
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 10:46:20 +0700

    Add object DestinasiEntry

---
+++ app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanEntry.kt
@@ -0,0 +1,9 @@
+package com.example.repositori.ui.theme.halaman
+
+import com.example.repositori.R
+import com.example.repositori.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
