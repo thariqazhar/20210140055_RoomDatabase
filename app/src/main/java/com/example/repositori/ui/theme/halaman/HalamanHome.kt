757319d5f7b8d5e8abd15605c5989cda962c9edc
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-06 15:37:27 +0700

    Add function BodyHome

--- app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/repositori/ui/theme/halaman/HalamanHome.kt
@@ -16,8 +16,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.repositori.R
 import com.example.repositori.data.Siswa
@@ -26,6 +29,30 @@ import com.example.repositori.navigasi.DestinasiNavigasi
 object DestinasiHome: DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+
+@Composable
+fun BodyHome(
+    itemSiswa : List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemSiswa.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.deskripsi_no_item),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        } else {
+            ListSiswa(
+                itemSiswa = itemSiswa,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
 }
 
 @Composable
